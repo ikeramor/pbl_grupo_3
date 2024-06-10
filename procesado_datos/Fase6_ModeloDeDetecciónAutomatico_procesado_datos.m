335c9a44d7b297c696a5828e940500c876711ed4
ikeramor <iker.amor@alumni.mondragon.edu>
2024-06-10 22:21:53 +0200

    update etiqueta

--- procesado_datos/Fase6_ModeloDeDetecciónAutomatico_procesado_datos.m
+++ procesado_datos/Fase6_ModeloDeDetecciónAutomatico_procesado_datos.m
@@ -66,6 +66,33 @@ for i=1:Diferencia
 end
 NGlaucoma=sum(TStratified.glaucoma==1)
 NSano=sum(TStratified.glaucoma==0)
+% Feature selection grafico
+
+Tstratified_variables.Media_Blue=TStratified.Media_Blue(:);
+Tstratified_variables.Desviac_Red=TStratified.Desviac_Red(:);
+Tstratified_variables.Desviac_Green=TStratified.Desviac_Green(:);
+Tstratified_variables.Contraste_wavelet_cH=TStratified.Contraste_wavelet_cH(:);
+Tstratified_variables.Homogeneidad_wavelet_cH=TStratified.Homogeneidad_wavelet_cH(:);
+Tstratified_variables.Homogeneidad_wavelet_cV=TStratified.Homogeneidad_wavelet_cV(:);
+Tstratified_variables.Homogeneidad_wavelet_cD=TStratified.Homogeneidad_wavelet_cD(:);
+Tstratified_variables.Entropia_wavelet_cV=TStratified.Entropia_wavelet_cV(:);
+Tstratified_variables.Ratio_DiscCup=TStratified.Ratio_DiscCup(:);
+%%
+Tstratified_variables.Desemejanza_wavelet_cD=TStratified.Desemejanza_wavelet_cD(:);
+%%
+Tstratified_variables.Glaucoma=TStratified.glaucoma;
+%%
+TStratifiedVariables=table(TStratified.Media_Blue,TStratified.Desviac_Red,TStratified.Desviac_Green,TStratified.Contraste_wavelet_cH,...
+    TStratified.Homogeneidad_wavelet_cH,TStratified.Homogeneidad_wavelet_cV,TStratified.Homogeneidad_wavelet_cD,...
+    TStratified.Entropia_wavelet_cV,TStratified.Ratio_DiscCup,TStratified.Desemejanza_wavelet_cD,TStratified.glaucoma,'VariableNames',{'Media_Blue','Desviac_Red','Desviac_Green',...
+    'Contraste_wavelet_cH','Homogeneidad_wavelet_cH','Homogeneidad_wavelet_cV','Homogeneidad_wavelet_cD','Entropia_wavelet_cV','Ratio_DiscCup','Desemejanza_wavelet_cD','glaucoma'})
+X = TStratifiedVariables(:, 1:end-1); % Variables predictoras
+y = TStratifiedVariables(:, end);     % Variable objetivo
+[idx,scores] = fscmrmr(X, y)
+bar(scores(idx))
+xlabel('Predictor rank')
+ylabel('Predictor importance score')
+xticklabels(strrep(TStratifiedVariables.Properties.VariableNames(idx),'_','\_'))
 %%
 %por medio de Classification Learner se ha desarrollado el modelo de
 %clasificacion
